f2780d9acdbef5a92da68f914682f99828a4fb70
Alfonso- LWA <alfonso@leapwithalice.io>
2018-04-12 02:27:43 -0400

    UI Changes and Coach marks, added site logo asset

---
+++ Leap with Alice Demo/Assets/Scripts/CoachesHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoachesHandler : MonoBehaviour {
+
+    public string[] coaches;
+    public bool deleteAll = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ClearCoaches ()
+    {
+        if(deleteAll)
+        {
+            PlayerPrefs.DeleteAll();
+        }
+        else
+        {
+            foreach (string str in coaches)
+            {
+                PlayerPrefs.SetInt(str, 0);
+            }
+        }
+
+        
+    }
+}
---
+++ Leap with Alice Demo/Assets/Scripts/DisableCoaches.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableCoaches : MonoBehaviour {
+
+    public string playerPrefsStorageName = "DONT FORGET TO CHANGE THIS";
+
+	// Use this for initialization
+	void Start () {
+
+        if (PlayerPrefs.GetInt(playerPrefsStorageName) == 1)
+        {
+            //Destroy(GameObject.Find("Coach"));
+            this.gameObject.SetActive(false);
+        }
+        else
+        {
+            PlayerPrefs.SetInt(playerPrefsStorageName, 1);
+            this.gameObject.SetActive(true);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
