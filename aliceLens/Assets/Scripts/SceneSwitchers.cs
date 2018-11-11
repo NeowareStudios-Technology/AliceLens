da15012cb23f5d4748dfa48641a8a1ceb9676260
Alfonso Morales <Alfonso@leapwithalice.io>
2018-11-11 18:32:13 -0500

    Alice Lens Update
     - Overhauled the app functionality and content
    - Contatins multiple scenes with differing image targets and content. The user can choose which scene to go to.
    - New Video playing system
    - Ability to pause and play video added

---
+++ aliceLens/Assets/Scripts/SceneSwitchers.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneSwitchers : MonoBehaviour {
+
+    public void SwitchtoAlice()
+    {
+        SceneManager.LoadScene("alice Lens");
+    }
+
+    public void SwitchtoHillel()
+    {
+        SceneManager.LoadScene("Hillel");
+    }
+
+    public void SwitchtoSaltofHeaven()
+    {
+        SceneManager.LoadScene("SaltofHeaven");
+    }
+
+    public void SwitchtoPaving()
+    {
+        SceneManager.LoadScene("PavingtheWay");
+    }
+
+    public void SwitchtoMontessori()
+    {
+        SceneManager.LoadScene("Montessori");
+    }
+
+    public void SwitchtoUCF()
+    {
+        SceneManager.LoadScene("UCF");
+    }
+}
